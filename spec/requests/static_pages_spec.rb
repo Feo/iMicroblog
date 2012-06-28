a48dbfb2877874bf612591ea6b6c87fafcf8dddd
Feo <jjzxcc@gmail.com>
2012-06-28 11:09:13 +0800

    Finish exercises 3.5.1 and 3.5.2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
 		it "should have the right title" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+			page.should have_selector('title', :text => "#{base_title} | Home")
 		end
 
     it "should have the content 'Sample App'" do
@@ -17,7 +20,7 @@ describe "StaticPages" do
 	describe "Help page" do
 		it "should have the right title" do
 			visit '/static_pages/help'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 
 		it "should have the content 'Help'" do
@@ -30,13 +33,27 @@ describe "StaticPages" do
 	describe "About page" do
 		it "should have the right title" do
 			visit '/static_pages/about'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About us")
+			page.should have_selector('title', :text => "#{base_title} | About us")
 		end
 
 		it "should have the content 'About'" do
 		
 			visit '/static_pages/about'
 			page.should have_content('About')
+		end
+	end
+
+	describe "Contact page" do
+		it "should have the right title" do
+			
+			visit "/static_pages/contact"
+			page.should have_selector('title', :text => "#{base_title} | Contact")
+		end
+
+		it "should have the content 'Contact'" do
+			
+			visit "/static_pages/contact"
+			page.should have_content('Contact')
 		end
 	end
 end
