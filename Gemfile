4ab4c5742c608716b89dabcb768bc50480cfbd50
Feo <jjzxcc@gmail.com>
2012-07-02 10:42:30 +0800

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.6'
 gem 'sqlite3'
 gem 'execjs'
 gem 'therubyracer'
+gem 'bootstrap-sass', '~> 2.0.4.0'
 
 
 # Gems used only for assets and not required
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Microblog::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+	root :to => "static_pages#home"
 
-	get "static_pages/about"
+	match '/help', :to => "static_pages#help"
+	match '/about', :to => "static_pages#about"
+	match '/contact', :to => "static_pages#contact"
 
-	get "static_pages/contact"
+	match '/signup', :to => "user#new"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
