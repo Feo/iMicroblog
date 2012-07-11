859742624f685c506bb15672ca5a3c792e729adf
Feo <jjzxcc@gmail.com>
2012-07-11 17:35:47 +0800

    Fix exercise 9.6.9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,9 +45,15 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		User.find(params[:id]).destroy
-		flash[:success] = "User destroyed."
-		redirect_to users_path
+		@user = User.find(params[:id])
+		if current_user != @user
+			@user.destroy
+			flash[:success] = "User destroyed."
+			redirect_to users_path
+		else
+			flash[:error] = "Cannot destroy yourself."
+			redirect_to users_path
+		end
 	end
 
 	private
