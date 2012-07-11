b96af3fef9e45d965969f44f4f6fedbbb92951f0
Feo <jjzxcc@gmail.com>
2012-07-11 14:11:18 +0800

    Finish exercise 9.6.1

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'bcrypt-ruby', '3.0.1'
 gem 'faker'
 gem 'will_paginate', '~>3.0.3'
 gem 'bootstrap-will_paginate', '~>0.0.7'
+gem 'haml'
 
 
 # Gems used only for assets and not required
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,6 +35,14 @@ describe User do
 		it { should be_admin }
 	end
 
+	describe "accessible attributes" do
+		it "should not allow access to admin" do
+			expect do
+				User.new(:name => "Example User", :email => "user@example.com", :password => "foobar", :password_confirmation => "foobar", :admin => true)
+			end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+		end
+	end
+
 	describe "when name is not present" do
 		before { @user.name = "" }
 		it { should_not be_valid }
