75f19ec202e9726142598e8dfaa495fcb1d9fb7f
Feo <jjzxcc@gmail.com>
2012-07-12 10:58:23 +0800

    Convert html to haml

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,14 +9,14 @@ describe "StaticPages" do
 		it { should have_selector("title", :text => full_title(page_title)) }
 	end
 
-  describe "Home page" do
+	describe "Home page" do
 		before { visit root_path }
 		let(:heading) { "Sample App" }
 		let(:page_title) { "" }
 
 		it_should_behave_like "all static pages"
 		it { should_not have_selector("title", :text => "| Home") }
-  end
+	end
  
 	describe "Help page" do
 		before { visit help_path }
