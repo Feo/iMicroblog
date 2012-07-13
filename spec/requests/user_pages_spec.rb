e644818c4fa27e0733091f9e9998e5c5d3d39311
Feo <jjzxcc@gmail.com>
2012-07-13 15:16:20 +0800

    Finish 10.3.2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,7 +53,7 @@ describe "UserPages" do
 	describe "signup page" do
 		before { visit signup_path }
 
-		it { should have_selector("h1", :text => "Sign Up") }
+		it { should have_selector("h1", :text => "Sign up") }
 		it { should have_selector("title", :text => full_title("Sign Up")) }
 	end
 
