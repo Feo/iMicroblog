4966ee34c07e9602217cdea8d165f0f944eecf52
Feo <jjzxcc@gmail.com>
2012-07-16 16:12:18 +0800

    Finish 10.5.7

---
+++ app/assets/javascripts/microposts.js
@@ -0,0 +1,10 @@
+function updateCountdown() {
+	var remaining = 140 - jQuery("#micropost_content").val().length;
+	jQuery('.countdown').text(remaining + " characters remaining");
+};
+
+jQuery(document).ready(function($){
+	updateCountdown();
+	$("#micropost_content").change(updateCountdown);
+	$("#micropost_content").keyup(updateCountdown);
+});
