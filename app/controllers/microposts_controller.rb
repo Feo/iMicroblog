3ee9fcece23404810879e2c1835d1dbe243d627a
Feo <jjzxcc@gmail.com>
2012-07-16 13:46:31 +0800

    Finish 10.4

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter :signed_in_user
+  before_filter :correct_user, :only => :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -13,5 +14,19 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_path
   end
+
+  def profile_destroy
+    @micropost.destroy
+    render current_user
+  end
+
+  private
+
+    def correct_user
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      redirect_to root_path if @micropost.nil?
+    end
 end
